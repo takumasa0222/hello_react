93931f8b3a368656a63d6eaebb2b2ed704642a69
takumasa0222 <t.tonnbo0222@gmail.com>
2025-05-26 07:01:23 +0900

    fixed typo

--- cdk/lib/constants/cloudfront.constants.ts
+++ cdk/lib/constants/cloudfront.constants.ts
@@ -1,5 +1,5 @@
 export const CLOUDFRONT = {
 	BASE_NAME: 'FrontendDistribution',
-	COMMENT: 'CDN for frontend content'
-	DEFAULT_OBJ: 'index.html'
+	COMMENT: 'CDN for frontend content',
+	DEFAULT_OBJ: 'index.html',
 }
