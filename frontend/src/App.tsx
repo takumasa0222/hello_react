dca7df548b15af958c51d3bb16c74a47db4f49c3
takumasa0222 <t.tonnbo0222@gmail.com>
2025-05-26 07:58:40 +0900

    add some debug code and modified front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import './App.css';
-import Greetings from './components/Greetings';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-function App() {
-	return (
-	  <div className="App">
-		<Greetings />
-	  </div>
-	);
-  }
-
-export default App;
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
