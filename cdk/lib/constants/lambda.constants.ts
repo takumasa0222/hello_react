f394f565912fc92f4471d9927d5639fa814f0703
takumasa0222 <t.tonnbo0222@gmail.com>
2025-05-26 22:44:22 +0900

    change direcdtory

--- cdk/lib/constants/lambda.constants.ts
+++ cdk/lib/constants/lambda.constants.ts
@@ -3,7 +3,7 @@ export const LAMBDA = {
 	HANDLER: 'index.handler',
 	TIMEOUT_SECONDS: 5,
 	MEMORY_MB: 128,
-	CODE_ASSET_PATH:'../backend/src'
+	CODE_ASSET_PATH:'../backend/dist'
 };
 
 export const LAMBDA_ENV = {
