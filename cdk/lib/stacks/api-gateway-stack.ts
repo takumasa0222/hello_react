1425dc281a04b1e51c41380f7b03e43e74dd80cf
takumasa0222 <t.tonnbo0222@gmail.com>
2025-05-27 09:25:21 +0900

    add debug log

--- cdk/lib/stacks/api-gateway-stack.ts
+++ cdk/lib/stacks/api-gateway-stack.ts
@@ -4,6 +4,8 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { StageStackProps } from "../types/stack-props";
 import { API_GATEWAY } from "../constants/api-gateway.constants";
 import { createResourceName } from "../utils/naming";
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as cdk from 'aws-cdk-lib';
 
 interface ApiGatwayStackProps extends StageStackProps {
 	lambdaFunction: lambda.Function;
@@ -19,20 +21,23 @@ export class ApiGatewayStack extends Construct {
 		this.restApi = new apigw.RestApi(this, 'RestApi', {
 			restApiName: apiGatewayName,
 			description: API_GATEWAY.DESCRIPTION,
-			// defaultCorsPreflightOptions: {
-			//   allowOrigins: apigw.Cors.ALL_ORIGINS,
-			//   allowMethods: apigw.Cors.ALL_METHODS,
-			// },
+			deployOptions: {
+				stageName: props.stage,
+				accessLogDestination: new apigw.LogGroupLogDestination(
+					new logs.LogGroup(this, 'ApiGatewayAccessLogs', {
+						retention: logs.RetentionDays.ONE_WEEK,
+					})
+				),
+				accessLogFormat: apigw.AccessLogFormat.jsonWithStandardFields(),
+
 		});
 		const message = this.restApi.root.addResource(API_GATEWAY.PATH);
-		if (!message.node.tryFindChild('OPTIONS')) {
-			message.addCorsPreflight({
-			  allowOrigins: API_GATEWAY.CORS_ORIGINS,
-			  allowMethods: API_GATEWAY.CORS_METHODS,
-			  allowHeaders: API_GATEWAY.CORS_HEADERS,
-			  allowCredentials: false,
-			});
-		}
+		message.addCorsPreflight({
+			allowOrigins: API_GATEWAY.CORS_ORIGINS,
+			allowMethods: API_GATEWAY.CORS_METHODS,
+			allowHeaders: API_GATEWAY.CORS_HEADERS,
+			allowCredentials: false,
+		});
 		message.addMethod(
 			API_GATEWAY.METHOD,
 			new apigw.LambdaIntegration(props.lambdaFunction, {
@@ -43,5 +48,20 @@ export class ApiGatewayStack extends Construct {
 				authorizationType: apigw.AuthorizationType.NONE,
 			}
 		);
+		new cdk.CfnOutput(this, 'ApiGatewayUrl', {
+			value: this.restApi.url,
+		});
+
+		new cdk.CfnOutput(this, 'FullMessageEndpoint', {
+			value: `${this.restApi.url}${API_GATEWAY.PATH}`,
+		});
+
+		new cdk.CfnOutput(this, 'DebugPath', {
+			value: API_GATEWAY.PATH,
+		});
+
+		new cdk.CfnOutput(this, 'DebugMethod', {
+			value: API_GATEWAY.METHOD,
+		});
 	}
 }
